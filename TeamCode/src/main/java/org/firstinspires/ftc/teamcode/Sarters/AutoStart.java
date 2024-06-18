ee46f677057cc1ed41911ebaf40abc27ca237a77
WizardMorpheus <charlie@ridgeon.com.au>
2024-06-18 12:02:11 +1000

    add autonomous and TeleOp starter code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sarters/AutoStart.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.Sarters;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous (group = "Starters", name = "AutoStarter")
+public class AutoStart extends LinearOpMode {
+    /**
+     * define the motors, servos, sensors and so forth that you will use throughout your OpMode here
+     * remember to import
+     */
+
+    /**
+     * this is the "main" function. it runs when you press the play button for this OpMode
+     */
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+
+    /**
+     * this is where you should define helper functions for your main code
+     */
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sarters/TeleOpStart.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.Sarters;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@TeleOp (group = "Starters", name = "AutoStarter")
+public class TeleOpStart extends OpMode {
+    /**
+     * define the motors, servos, sensors and so forth that you will use throughout your OpMode here
+     * remember to import
+     */
+
+    /**
+     * this is where you should initialise all of your variables you just defined, this is what runs
+     * when you press the init button
+     */
+    @Override
+    public void init() {
+
+    }
+
+    /**
+     * this is the "main" function. it runs when you press the play button for this OpMode
+     * it is a loop so will run continuously until you press stop
+     */
+    @Override
+    public void loop() {
+
+    }
+
+    /**
+     * this is where you should define helper functions for your main code
+     */
+}
